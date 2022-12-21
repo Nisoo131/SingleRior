b15ef0407439a49f69ac42c58676f9aa059a335f
Nisoo131 <skypass8632@gmail.com>
2022-12-22 02:33:26 +0900

    스토어 장바구니

---
+++ SingleRior_Project/src/main/java/com/E1I4/project/storeBoard/model/vo/Cart.java
@@ -0,0 +1,24 @@
+package com.E1I4.project.storeBoard.model.vo;
+
+import java.sql.Date;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+public class Cart {
+	// cart
+	private int cartNo;
+	private int quantity;
+	private String payStatus;
+	private String userId;
+	private int productNo;
+
+}
